8b9ce51e883f44d0640083bf111d6b352e7df234
De-lovely-L <2521795828@qq.com>
2018-10-11 18:17:02 +0800

    modified notFound.vue

--- vueProject/src/view/home.vue
+++ vueProject/src/view/home.vue
@@ -2,14 +2,14 @@
 	<div>
 		<header-vue :title="title"></header-vue>
 		<div class="menu">
-			<Menu :theme="theme1" active-name="1" @on-select="handleSelect">
-		            <MenuItem name="0" >
+			<Menu :theme="theme1" active-name="activeName">
+		            <MenuItem name="0">
 		                <!-- <router-link :to="{name:'first'}">{{item.title}}</router-link> -->
-										<router-link :to="{name:'first'}">文章管理</router-link>
+						<router-link :to="{name:'first'}">文章管理</router-link>
 		            </MenuItem>
-								<MenuItem name="1" >
+					<MenuItem name="1">
 		                <!-- <router-link :to="{name:'first'}">{{item.title}}</router-link> -->
-										<router-link :to="{name:'second'}">评论管理</router-link>
+						<router-link :to="{name:'second'}">评论管理</router-link>
 		            </MenuItem>
 	    	</Menu>
     	</div>
@@ -25,7 +25,7 @@
 <script>
 //引入子组件对象
 import headerVue from '@/view/header.vue'
-
+import firstVue from '@/components/first.vue'
 	export default{
 		name:'home',
 		data(){
@@ -37,17 +37,16 @@ import headerVue from '@/view/header.vue'
 					{'title': '评论管理','type': 'ios-paper', 'link': 'second'},
 					{'title': '用户留存','type': 'ios-people', 'link': 'shop'},
 					{'title': '流失用户','type': 'ios-analytics','link': 'fourth'},
-				]
+				],
+				activeName:0
 			}
 		},
 		methods:{
-			handleSelect(name){
-			
-     		},	
 		},
 		components:{
 			//组件名（在模板中使用）： 组件对象
-			headerVue
+			headerVue,
+			firstVue
 		},
 		created(){
 			console.log(this.$http);
--- vueProject/src/view/study/mintStudy.vue
+++ vueProject/src/view/study/mintStudy.vue
@@ -8,7 +8,8 @@
 		  <mt-button icon="more" slot="right"></mt-button>
 		</mt-header>
 		
-		
+		<Table class="table" :columns="columns1" :data="data1"></Table>
+        <Table class="table" :columns="columns1" :data="data1"></Table>
 		
 		<mt-swipe :auto="1000">
 		  <mt-swipe-item>1</mt-swipe-item>
@@ -23,7 +24,46 @@
 		name:'mintUi',
 		data(){
 			return{
-				
+				 columns1: [
+                    {
+                        title: 'Name',
+                        key: 'name'
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age'
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    }
+            ],
+            data1: [
+                {
+                    name: 'John Brown',
+                    age: 18,
+                    address: 'New York No. 1 Lake Park',
+                    date: '2016-10-03'
+                },
+                {
+                    name: 'Jim Green',
+                    age: 24,
+                    address: 'London No. 1 Lake Park',
+                    date: '2016-10-01'
+                },
+                {
+                    name: 'Joe Black',
+                    age: 30,
+                    address: 'Sydney No. 1 Lake Park',
+                    date: '2016-10-02'
+                },
+                {
+                    name: 'Jon Snow',
+                    age: 26,
+                    address: 'Ottawa No. 2 Lake Park',
+                    date: '2016-10-04'
+                }
+            ]
 			}
 		},
 		methods:{
@@ -35,4 +75,9 @@
 </script>
 
 <style>
+	.table{
+        display: inline-block;
+        width: 50%;
+        float: left;
+    }
 </style>
